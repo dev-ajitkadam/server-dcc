ff0d8bfc135f649b988243a37f8d42ba16d5e6fe
Ajit kadam <111628934+dev-ajitkadam@users.noreply.github.com>
2024-08-18 15:45:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const scheduleRoutes = require("./Routers/scheduleRoute")
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://dev-ajitkadam.github.io/*'],  // Replace with your actual domains
+    origin: '*',  // Allow requests from any origin
     credentials: true,  // Allow cookies and other credentials to be sent
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization'
